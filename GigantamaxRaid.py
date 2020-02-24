4b5007b104723f646fb3480c70f90be71ee5ef32
Daniel <danybeam@gmail.com>
2020-02-23 17:42:29 -0800

    finish switch day and return

--- GigantamaxRaid.py
+++ GigantamaxRaid.py
@@ -1,5 +1,6 @@
 import argparse
 import asyncio
+from datetime import date
 import logging
 import os
 from contextlib import contextmanager
@@ -9,11 +10,123 @@ from joycontrol.controller_state import ControllerState, button_push
 from joycontrol.protocol import controller_protocol_factory, Controller
 from joycontrol.server import create_hid_server
 
-from ShinyRaid import shinyRaid
-
 logger = logging.getLogger(__name__)
 
-async def gigantamaxRaid(controller_state: ControllerState,connected=False):
+today = date.today()
+current_switch_date = [int(today.year),int(today.month),int(today.day)]
+months_31={1,3,5,7,8,10,12}
+months_30={4,6,9,11}
+
+def isLeap(year: int):
+
+
+# NOTE there IS a way to force shiny raids but it's not compatible with this particular rolling method
+async def switchDay(controller_state: ControllerState, connected=False):
+    pass
+
+# FINISHED
+async def switchDayAndReturn(controller_state: ControllerState, connected=False, first=True):
+    if not connected:
+        await controller_state.connect() # asuming from sync controller
+
+        # goto game
+        print("get out of controller")
+        await button_push(controller_state, 'home')
+        await asyncio.sleep(1)
+        print("enter game")
+        await button_push(controller_state, 'home')
+        await asyncio.sleep(2)
+
+    # exit game
+    print("going home")
+    await button_push(controller_state, 'home')
+    await asyncio.sleep(0.3)
+    # go to settings  
+    print("BBS")
+    await button_push(controller_state, 'down')
+    await asyncio.sleep(0.3)
+    print("Settings")
+    await button_push(controller_state, 'right', sec=0.5)
+    await button_push(controller_state, 'left')
+    await asyncio.sleep(0.3)
+    print("enter settings")
+    await button_push(controller_state, 'a')
+    await asyncio.sleep(0.5)
+    print("system")
+    await button_push(controller_state, 'down', sec=1.6)
+    await asyncio.sleep(0.3)
+    await button_push(controller_state, 'a')
+    await asyncio.sleep(0.3)
+    print("date and time")
+    await button_push(controller_state, 'down')
+    await asyncio.sleep(0.3)
+    await button_push(controller_state, 'down')
+    await asyncio.sleep(0.3)
+    await button_push(controller_state, 'down')
+    await asyncio.sleep(0.3)
+    await button_push(controller_state, 'down')
+    await asyncio.sleep(0.3)
+    await button_push(controller_state, 'a')
+    await asyncio.sleep(0.3)
+    print("date and time setting")
+    await button_push(controller_state, 'down',sec=0.5)
+    await button_push(controller_state, 'a')
+    await asyncio.sleep(1)
+    # connect to the internet and disconnect to get current day
+    if first:
+        today = date.today()
+        current_switch_date = [int(today.year),int(today.month),int(today.day)]
+        print("sync with internet")
+        await button_push(controller_state, 'a')
+        await asyncio.sleep(0.5)
+        print("unsync from internet")
+        await button_push(controller_state, 'a')
+        await asyncio.sleep(0.5)
+    # switch day
+    await switchDay(controller_state,connected=True)
+    # return home
+    print("return home")
+    await button_push(controller_state, 'home')
+    await asyncio.sleep(2)
+    # return to game
+    print("enter game")
+    await button_push(controller_state, 'home')
+    await asyncio.sleep(2)
+
+# FINISHED
+async def restartGame(controller_state: ControllerState, connected=False):
+    if not connected:
+        await controller_state.connect() # asuming from sync controller
+
+        # goto game
+        print("get out of controller")
+        await button_push(controller_state, 'home')
+        await asyncio.sleep(1)
+        print("enter game")
+        await button_push(controller_state, 'home')
+        await asyncio.sleep(2)
+    
+    #Get out
+    print("going home")
+    await button_push(controller_state, 'home')
+    await asyncio.sleep(1)
+    print("Close game")
+    await button_push(controller_state, 'x')
+    await asyncio.sleep(0.8)
+    print("confirm close")
+    await button_push(controller_state, 'a')
+    await asyncio.sleep(3)
+    print("open again")
+    await button_push(controller_state, 'a')
+    await asyncio.sleep(1)
+    print("choose profile")
+    await button_push(controller_state, 'a') # asuming you have to chose a profile
+    await asyncio.sleep(17.5) # wait for loading and gaining control back
+    print("start screen")
+    await button_push(controller_state, 'a')
+
+# FINISHED
+async def purpleBeam(controller_state: ControllerState,connected=False):
     if not connected:
         await controller_state.connect() # asuming from sync controller
 
@@ -26,21 +139,20 @@ async def gigantamaxRaid(controller_state: ControllerState,connected=False):
         await asyncio.sleep(2)
 
 
-        # asuming in front of den
-        # safety save
-        print("pause menu")
-        await button_push(controller_state, 'x')
-        await asyncio.sleep(1)
-        print("save")
-        await button_push(controller_state, 'r')
-        await asyncio.sleep(2)
-        print("confirm save")
-        await button_push(controller_state, 'a')
-        await asyncio.sleep(4)
+    # asuming in front of den
+    # safety save
+    print("pause menu")
+    await button_push(controller_state, 'x')
+    await asyncio.sleep(1)
+    print("save")
+    await button_push(controller_state, 'r')
+    await asyncio.sleep(2)
+    print("confirm save")
+    await button_push(controller_state, 'a')
+    await asyncio.sleep(4)
 
     # check for gigantamax pkmn in loop
-    keepLooking = True
-    while keepLooking:
+    while True:
         print("trigger wishing piece")
         await button_push(controller_state, 'a')
         await asyncio.sleep(1)
@@ -55,37 +167,40 @@ async def gigantamaxRaid(controller_state: ControllerState,connected=False):
         check = input("Was it purple? ")
         if check == "y" or check == "yes":
             break
-        # so far so good
-        print("Close game")
-        await button_push(controller_state, 'x')
-        await asyncio.sleep(0.8)
-        print("confirm close")
-        await button_push(controller_state, 'a')
-        await asyncio.sleep(3)
-        print("open again")
-        await button_push(controller_state, 'a')
-        await asyncio.sleep(1)
-        print("choose profile")
-        await button_push(controller_state, 'a') # asuming you have to chose a profile
-        await asyncio.sleep(17.5) # wait for loading and gaining control back
-        print("start screen")
-        await button_push(controller_state, 'a')
+        print("restart game")
+        await restartGame(controller_state,connected=True)
         await asyncio.sleep(10)
 
-    check = input("force shiny?")
-    if check == "y" or check == "yes": # async def test_controller_buttons(controller_state: ControllerState):
-        shinyRaid(controller_state, connected=True)
+    first=True
+    while True:
+        # enter den check pkmn
+        print("enter den")
+        await button_push(controller_state, 'a')
+        check = input("Was it gigantamax? ")
+        if check == "y" or check == "yes":
+            break
+        print("invite others")
+        await button_push(controller_state, 'a')
+        await asyncio.sleep(1.5)
+        print("switch day and return")
+        await switchDayAndReturn(controller_state, connected=True,first=first)
+        first=False
+        print("exit raid")
+        await button_push(controller_state, 'b')
+        await asyncio.sleep(1.5)
+        print("confirm exit")
+        await button_push(controller_state, 'a')
+        await asyncio.sleep(4)
 
 async def _main(controller, capture_file=None, spi_flash=None):
     factory = controller_protocol_factory(controller, spi_flash=spi_flash)
     transport, protocol = await create_hid_server(factory, 17, 19, capture_file=capture_file)
     controller_state = protocol.get_controller_state()
 
-    await gigantamaxRaid(controller_state)
+    await purpleBeam(controller_state)
 
     logger.info('Stopping communication...')
     await transport.close()
-
 
 if __name__ == '__main__':
     # check if root
