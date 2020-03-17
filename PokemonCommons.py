93d00a9f00b5224e668734213593006896a8e6d0
Daniel Orozco <danybeam@gmail.com>
2020-03-16 20:07:27 -0700

    setup

--- PokemonCommons.py
+++ PokemonCommons.py
@@ -94,7 +94,7 @@ async def switchDay(controller_state: ControllerState, connected=False):
         current_switch_date[YEAR] += 1
 
     # press right 1+ second
-    await button_push(controller_state, 'right', sec=2)
+    await button_push(controller_state, 'right', sec=1.5)
     await asyncio.sleep(0.5)
     await button_push(controller_state, 'left')
     await asyncio.sleep(0.5)
@@ -153,9 +153,9 @@ async def switchDayAndReturn(controller_state: ControllerState, connected=False,
         #today = date.today()
         # current_switch_date = [
         #    int(today.year), int(today.month), int(today.day)]
-        current_switch_date[DAY] = 29
-        current_switch_date[MONTH] = 2
-        current_switch_date[YEAR] = 2032
+        current_switch_date[DAY] = 17
+        current_switch_date[MONTH] = 3
+        current_switch_date[YEAR] = 2020
         #print("sync with internet")
         # await button_push(controller_state, 'a')
         # await asyncio.sleep(1)
