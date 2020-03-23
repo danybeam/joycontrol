0ddc0b113a60f3bc7a60056c3e693fe7f5447a51
Daniel Orozco <danybeam@gmail.com>
2020-03-22 21:25:34 -0700

    desync fix

--- RottoLotto.py
+++ RottoLotto.py
@@ -98,12 +98,9 @@ async def rottoLotto(controller_state: ControllerState, connected=False, first=T
     print('[EXIT TEXT]')
     await asyncio.sleep(2)
     # A(exit texxt)
-    await button_push(controller_state, 'b')
-    await asyncio.sleep(0.5)
-    await button_push(controller_state, 'b')
-    await asyncio.sleep(0.5)
-    await button_push(controller_state, 'b')
-    await asyncio.sleep(0.5)
+    for _ in range(10):
+        await button_push(controller_state, 'b')
+        await asyncio.sleep(0.3)
     print('clean text buffer')
     # CHANGE DAY
     await switchDayAndReturn(controller_state, connected=True, first=first)
